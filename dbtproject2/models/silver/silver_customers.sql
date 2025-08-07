5d193b4f333b5f74c491c6d9fdd8815d43021745
varsha1994-vk <smitha.varshiha20@gmail.com>
2025-08-07 18:09:04 -0400

    This is my second commit

--- dbtproject2/models/silver/silver_customers.sql
+++ dbtproject2/models/silver/silver_customers.sql
@@ -6,8 +6,12 @@
 }}
 
 WITH CTE_customer AS(
-    SELECT
-         * FROM SESSION13.SESSION13SCH_bronze.RAW_CUSTOMERS
+    SELECT * FROM SESSION13.SESSION13SCH_bronze.RAW_CUSTOMERS
 )
 
-SELECT * FROM CTE_CUSTOMER
+SELECT CUSTOMER_ID,
+    initcap(name) as name,
+    age,
+    initcap(city) as city,
+    CASE WHEN age >18 THEN 'ADULT' ELSE 'MINOR' END AS AGE_GROUP
+    FROM CTE_CUSTOMER
